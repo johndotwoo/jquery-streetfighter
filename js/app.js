32540c2ae94cdd16cdd5ef222fea0c33cf50e169
johndotwoo <johndotwoo@hotmail.com>
2014-05-11 14:49:46 -0400

    added jQuery user click handler
    coded .hide() and .show() behavior, animated the Hadouken and added the
    "Hadouken!" sound

--- js/app.js
+++ js/app.js
@@ -2,20 +2,33 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	});
+	})
 	$('.ryu').mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	});
+	})
 	$('.ryu').mousedown(function() {
-		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// animate hadouken to the right of the screen
-	});
+		$('.hadouken').finish().show() 
+		.animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
+	})
 	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
